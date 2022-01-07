5a44b72fefbaa89fa8732ffe92723bfcd520646c
Jack Hill <73407206+JackIHill@users.noreply.github.com>
2022-01-07 20:55:42 +0000

    Refactoring updates (#26)
    Much improved and consistent function docstrings.
    color keyword argument added to plot_variables() to manually change the color of any or all taxa for the current plot only.
    set_colors() function to manually change default taxon-color mapping behaviour.
    Better save details text file formatting.
    README updated with example usage and example output, and other misc formatting/grammatical changes.
    
    README updates co-authored-by: alkrost <97312338+alkrost@users.noreply.github.com>

--- README.md
+++ README.md
@@ -3,43 +3,96 @@ Create plots for cerebellar and cerebral morphology in a range of primates.
 Run alongside csv file containing brain morphological data from a range of studies.
 CSV will be updated in the future as the project progresses.
 
-<ins>Functions<ins>
+- [About the Cerebellum Project](#about-the-cerebellum-project)
+
+## Usage
     
-**plot_variables():**<br>
-Produces plots with legend, data point colours mapped to taxon.
-- Keyword Arguments:
-    - xy -- str. Tuple of tuples each containing variable pairs. default=all combinations for the relevant data
-    - logged -- bool. If True, produces logged plots for xy. default=False.
-    - save -- bool. If True, creates either 'Saved Simple Plots' or 'Saved Log Plots' folders depending on if the 'logged' keyword argument is specified as True or False. 
-        - A text file is created in each of these folders with details on what each plot file contains. 
-        - This is because including variable names in file name could cause confusion/more clutter than necessary.
-    - show -- bool. If True, outputs plots to a new window. 
-<br><br>
-- Example Input:
-    ```
-    plot_variables(
-                (('Cerebrum Volume', 'Cerebellum Volume'),),  
-                logged=True, save=True, show=True
-                )  
-    ```
+```plot_variables()``` produces plots with a legend and data point colours mapped to taxa.
+<br>
+<br>
+Calling ```plot_variables()``` without specifying a) a tuple of tuples containing indepdent and dependent variable pairs or b) a list of .csv column indices, will create a default figure containing plots derived from pairwise, non-repeated combinations of csv columns 4, 3 and 1 (ordered for aesthetic purposes) - those with sufficient data points. Thus:
+<br>
 
-**plot_regression():**<br>
-Plots linear regression line for the volume-against-volume plot.
+```python
+plot_variables(show=True)
+```
+Produces: 
 
+![default_plot_variables](https://user-images.githubusercontent.com/73407206/148590626-292c2844-1c0c-40e0-817a-452dde6c739f.png)
 
-**delete_folder():**<br>
-Deletes simple or log save folder.
-- Keyword Arguments:
-    - logged -- bool. If True, deletes log save folder. default=False (deletes simple save folder).<br>
+<br>
+
+Specifying n number of variable combination tuples will plot n number of plots on the figure. ```logged=True``` can also be passed, to log every plot in the figure. Thus:
+
+```python
+plot_variables(
+    (('Cerebrum Volume', 'Cerebellum Volume'),),  
+    logged=True, show=True
+    )  
+```
+
+Produces:
+<br>
+<br>
+![logged_cbvol_cbllum_vol](https://user-images.githubusercontent.com/73407206/148590809-855fe955-aaf0-42dd-8a32-b8e9736cbae8.png)
+
+<br>
+Specify a list of column indices to plot all non-repeated combinations of the variables those columns represent.
+
+```python
+plot_variables([1, 2, 3, 4], show=True)
+```
+
+will therefore produce 6 plots; variable combinations:
+```(1, 2)```, ```(1, 3)```, ```(1, 4)```, ```(2, 3)```, ```(2, 4)```, ```(3, 4)``` where:
+
+<br>
+
+column index 1 = Cerebellum Surface Area <br>
+column index 2 = Cerebrum Surface Area <br>
+column index 3 = Cerebellum Volume <br>
+column index 4 = Cerebrum Volume <br>
+
+<br>
+
+Manually change the color map for the current plot using the ```colors``` keyword. Pass a dictionary containing a taxon name key and a color value, where valid taxa are 'Hominidae', 'Hylobatidae', 'Cercopithecidae' and 'Platyrrhini' and valid colors are matplotlib named colors (https://matplotlib.org/stable/gallery/color/named_colors.html) or hex color codes. Thus:
+
+```python
+plot_variables(colors={'Hominidae':'blue'}, show=True)
+``` 
+
+will modify the color map for the taxon Hominidae, leaving all other taxon color maps as their default, as such:
+
+![default_plot_colored_diff](https://user-images.githubusercontent.com/73407206/148591146-3494d9ef-c56a-4fcd-90a9-a2fac7fb887f.png)
+
+To set custom color maps for any or all taxon for all subsequent plots, pass a dictionary of 'taxon_name':'color' to ```set_colors()```:
+
+<br>
+
+```python
+plot_variables(show=True) # plot all taxon with module-defined default color map
+plot_variables(colors={'Hylobatidae':'blue'}, show=True) # change plot's Hylobatidae colors, other taxa have default colors
+
+set_colors({'Hominidae':'red'}) # set default color for Hominidae plot points, for all subsequent plots. 
     
+plot_variables(show=True) # default color for Hominidae is red, all other colors are original (Hylobatidae no longer blue). 
+```
 
- <br>
+<br>
 
-**<ins>ABOUT THE CEREBELLUM PROJECT<ins>**
+Passing ```save=True``` Will create a new directory inside the current directory, and store the current figure in 'Saved Simple Plots' or 'Saved Log Plots' depending on if ```logged``` is True. SIMPLE / LOG_PLOT_DETAILS.txt files will be created simultaneously, providing information for each figure on the number of plots, save file order, variables used for each plot, and the time at figure creation. 
 
-The cerebellum project studies cerebellar evolution at microscopic scale, across several primate species. It aims at creating precise delineations of the pial surface of the cerebellar cortex. With this, we measure the surface area and volume of each cerebellum and cerebrum in each primate brain. We will also attempt to use the annotations to generate 3D volumes and 3D meshes of the gorilla cerebellum. Eventually we will use these surface data to guide segmentation into cerebellar sub-structures with more precise functions.
+To easily delete these folders, ```delete_folders(logged=True)``` for the 'Saved Log Plots' directory, and ```delete_folders()``` for the 'Saved Simple Plots' directory.
 
-Primarily, for annotation, we use MicroDraw, available at: https://microdraw.pasteur.fr/
+<br>
+
+```plot_regression()``` plots linear regression line for the volume-against-volume plot.
+
+## About the Cerebellum Project
+
+The cerebellum project studies cerebellar evolution at microscopic scale, across several primate species. It aims at creating precise delineations of the pial surface of the cerebellar cortex. With this, we measure the surface area and volume of each cerebellum and cerebrum in each primate brain. We will also attempt to use the annotations to generate 3D volumes and 3D meshes of the gorilla cerebellum (for now, with more species to come). Eventually we will use these surface data to guide segmentation into cerebellar sub-structures with more precise functions.
+
+Primarily, for annotation, we use MicroDraw, available at: https://microdraw.pasteur.fr/ and https://github.com/r03ert0/microdraw
 
 To learn how we annotate each structure, visit: https://docs.google.com/document/d/1gAQIiEM9bAYKqiLpG9vHDmuaBk-LYpSIdsVsISAPzq4/edit
 
@@ -48,4 +101,3 @@ To learn how we annotate each structure, visit: https://docs.google.com/document
 See our prototype reconstruction of an eastern lowland gorilla cerebellum:
 
 ![Gorilla3DTransparent](https://user-images.githubusercontent.com/73407206/136446331-42e5afb3-2867-4329-952f-3b5593972e9c.gif)
-
--- cbpmodels.py
+++ cbpmodels.py
@@ -6,8 +6,8 @@ You will be given the option to save simple and logged plots (to separate folder
 
 # TODO:
 #  1) Add logging and unit testing
-#  2) Could separate scaling definitions into its own func. At least, do something about handles and colors globals
-#  3) amend var_combinations() comment (as 'pairs' kwarg can now be used)
+#  2) amend module docstring
+#  3) implement threading due to plot_variables inner function calls
 
 import os
 import sys
@@ -25,13 +25,14 @@ from matplotlib.lines import Line2D
 try:
     data = pd.read_csv('all_species_values.csv', na_values='', usecols=range(7))
     data = data.dropna(how='all', axis='columns').drop(columns='Source')
-    data.rename(columns={
-        'Species ': 'Species',
-        'CerebellumSurfaceArea': 'Cerebellum Surface Area',
-        'CerebrumSurfaceArea': 'Cerebrum Surface Area',
-        'CerebellumVolume ': 'Cerebellum Volume',
-        'CerebrumVolume': 'Cerebrum Volume'
-        }, inplace=True)
+    data.rename(
+        columns={
+            'Species ': 'Species',
+            'CerebellumSurfaceArea': 'Cerebellum Surface Area',
+            'CerebrumSurfaceArea': 'Cerebrum Surface Area',
+            'CerebellumVolume ': 'Cerebellum Volume',
+            'CerebrumVolume': 'Cerebrum Volume'
+            }, inplace=True)
 
 except FileNotFoundError:
     print('CSV not found. Please ensure you have the \'all_species_values.csv\' '
@@ -41,55 +42,84 @@ except FileNotFoundError:
 
 # Column 3 (Cerebrum Surface Area) is not plotted due to not enough data.
 # Add '2' to col_names list below if want to include. Column names extracted in order 4, 3, 1 for figure aesthetics.
-def var_combinations(cols=[4, 3, 1]):
-    """
-    Get all combinations (not repeated) for a list of columns.
+def var_combinations(cols):
+    """Get all combinations (not repeated) for a list of columns.
 
-    Parameters:
-    cols (list): list of integers representing column index values from all_species_values.csv. 
-                 defaults to columns 4, 3, and 1 (Cerebrum Volume, Cerebellum Volume, Cerebellum Surface Area).
-                 Order is 4, 3, 1 for plot aesthetic purposes only. 
-                 Column 2, Cerebrum Surface Area is omitted due to lack of data. 
+    Args:
+        cols (list of int): list of integers representing column index values from .csv. 
 
-    Return Value:
-    var_combinations (tuple): tuple of tuples, each containing independent/dependent variable pairs.
+    Returns:
+        tuple: tuple of tuples, each containing independent/dependent variable pairs.
     """
     var_combinations = tuple(combinations(data.columns[cols], 2))
     return var_combinations
 
+default_colors = {
+            'Hominidae': '#7f48b5',
+            'Hylobatidae': '#c195ed',
+            'Cercopithecidae': '#f0bb3e',
+            'Platyrrhini': '#f2e3bd'
+            } 
 
-def set_colors(Hominidae='#7f48b5', Hylobatidae='#c195ed', Cercopithecidae='#f0bb3e', Platyrrhini='#f2e3bd'):
-    """
-    Assign custom colors to species for visualisation purposes.
+def set_colors(new_colors=None):
+    """Assign custom colors to species for visualisation purposes.
 
-    Parameters:
-    Hominidae (str): takes matplotlib named colors or hex color codes for Hominidae (Great Apes).
-    Hylobatidae (str): takes matplotlib named colors or hex color codes for Hylobatidae (Gibbons).
-    Cercopithecidae (str): takes matplotlib named colors or hex color codes for Cercopithecidae (Old World Monkey).
-    Platyrrhini (str): takes matplotlib named colors or hex color codes for Platyrrhini (New World Monkey).
+    Args:
+        new_colors (dict of str:str, optional): species:color dictionary where valid species names are:
+            'Hominidae', 'Hylobatidae', 'Cercopithecidae' or 'Platyrrhini' and valid colors are matplotlib
+            named colors or hex color codes. Defaults to empty dictionary.
+            
+    Returns:
+        default_colors (dict of str: str): default_colors dict merged with values from new_colors. 
 
     matplotlib named colors: https://matplotlib.org/stable/gallery/color/named_colors.html
     """
-    colors = {
-        'Hominidae': Hominidae,
-        'Hylobatidae': Hylobatidae,
-        'Cercopithecidae': Cercopithecidae,
-        'Platyrrhini': Platyrrhini
-        } 
-    return colors
-       
+    if new_colors is None:
+        new_colors = {}
 
-def plot_variables(xy=var_combinations(), colors=set_colors(), pairs=[], logged=False, save=False, show=False):
-    """
-    - Plots brain morphology variables.
-    - Pass no arguments to plot 3 default plots.
-    - Pass logged=True with no arguments to log these default plots.
-    - Pass a tuple containing tuples which contain variable pairs to plot custom variables, like so:
-    - plot_variables((('Cerebrum Volume', 'Cerebellum Volume'),)).
+    default_colors.update(new_colors)
+    return default_colors
+
+
+def plot_variables(xy=None, colors=None, logged=False, save=False, show=False):
+    """Plots brain morphology variables
+
+    Args:
+        xy (tuple or list, optional): accepts tuple of tuples, each containing independent/dependent variable pairs, 
+            or a list of integers representing column index values from .csv, to be passed to var_combinations().
+            Ensure a minimum of two integers are defined. Defaults to var_combinations([4, 3, 1]).
+        colors (dict of str: str), optional): species:color dictionary where valid species names are:
+            'Hominidae', 'Hylobatidae', 'Cercopithecidae' or 'Platyrrhini' and valid colors are matplotlib
+            named colors or hex color codes. Maps the respective colors to each species' plot markers. Defaults to: 
+            {'Hominidae': '#7f48b5', 'Hylobatidae': '#c195ed', 'Cercopithecidae': '#f0bb3e', 'Platyrrhini': '#f2e3bd'}.
+        logged (bool, optional): produce simple (unlogged, False) or logged plots (True). Defaults to False.
+        save (bool, optional): if True, save figure to 'Saved Simple Plots' or 'Saved Log Plots' folders, 
+            respective of `logged`. Passes xy (tuple, optional) to save_plots() to provide detailed file names, 
+            and SIMPLE/LOG_PLOT_DETAILSD.txt files to the respective save folder. Defaults to False.
+        show (bool, optional): if True, call plt.show() to output figures to new windows. Defaults to False.
     """
 
-    if pairs:
-        xy = var_combinations(pairs)
+    if xy is None:
+        xy = var_combinations([4, 3, 1])
+    elif isinstance(xy, list):
+        try:
+            if all(0 < i <= 4 for i in xy):
+                xy = var_combinations(xy)
+            else:
+                print('\nAn invalid index value was passed to the `xy` keyword argument, and so the default'
+                    ' configuration of variables was plotted. Ensure that index values are not lower than 1'
+                    ' and do not exceed 4.\n')
+
+        except TypeError as t:
+            print('\nA list containing integers was not passed to `xy`, therefore the default configuration'
+                ' of variables was plotted. Ensure integers are not lower than 1 and do not exceed 4\n')
+
+    if colors is None:
+        colors = default_colors
+    else:
+        def_copy = default_colors.copy()
+        def_copy.update(colors)
+        colors = def_copy
 
     # Define scaling properties for each number of axes in a figure.
     left_margin = 2.5
@@ -119,20 +149,20 @@ def plot_variables(xy=var_combinations(), colors=set_colors(), pairs=[], logged=
 
     # Give legend markers same color as predefined taxon colors. Marker placed on white line. 
     handles = [
-    Line2D([0], [0],
-           color='w', marker='o', markerfacecolor=v,
-           markeredgecolor='k', markeredgewidth='0.5',
-           markersize=4, label=k,
-           ) for k, v in colors.items()
-          ]
+        Line2D([0], [0],
+        color='w', marker='o', markerfacecolor=v,
+        markeredgecolor='k', markeredgewidth='0.5',
+        markersize=4, label=k,
+        ) for k, v in colors.items()
+        ]
     
-    fig1, axs1 = plt.subplots(rows, cols, figsize=(fig_width, 4), squeeze=False)
-    axs1 = axs1.flatten()
+    fig, axs = plt.subplots(rows, cols, figsize=(fig_width, 4), squeeze=False)
+    axs = axs.flatten()
 
     for i, (x, y) in enumerate(xy):
-        axs1[i].scatter(data[x], data[y], c=data.Taxon.map(colors), edgecolor='k')
+        axs[i].scatter(data[x], data[y], c=data.Taxon.map(colors), edgecolor='k')
 
-        ax_legend = axs1[i].legend(
+        ax_legend = axs[i].legend(
             title='Taxon',
             title_fontsize='9',
             handles=handles,
@@ -142,37 +172,37 @@ def plot_variables(xy=var_combinations(), colors=set_colors(), pairs=[], logged=
         ax_legend.get_frame().set_color('white')
 
         if not logged:
-            axs1[i].set(
+            axs[i].set(
                 title=f'Primate {xy[i][0]} against\n{xy[i][1]}',
                 xlabel=f'{xy[i][0]}',
                 ylabel=f'{xy[i][1]}'
                 )
         
         elif logged:
-            axs1[i].set(
+            axs[i].set(
                 title=f'Logged Primate {xy[i][0]} against\n{xy[i][1]}',
                 xlabel=f'Logged {xy[i][0]}',
                 ylabel=f'Logged {xy[i][1]}'
                 )
             
-            axs1[i].set_xscale('log')
-            axs1[i].get_xaxis().set_major_formatter(tk.ScalarFormatter())
+            axs[i].set_xscale('log')
+            axs[i].get_xaxis().set_major_formatter(tk.ScalarFormatter())
 
-            axs1[i].set_yscale('log')   
-            axs1[i].get_yaxis().set_major_formatter(tk.ScalarFormatter())
-            axs1[i].set_yticks([10, 25, 50, 100, 250, 500, 1000])
+            axs[i].set_yscale('log')   
+            axs[i].get_yaxis().set_major_formatter(tk.ScalarFormatter())
+            axs[i].set_yticks([10, 25, 50, 100, 250, 500, 1000])
 
             # These variables need custom xticks to better represent the range of values.
             tick_list = [5, 10, 25, 50, 100, 200, 400, 1000]
             if (x, y) == ('Cerebrum Volume', 'Cerebellum Volume'):
-                axs1[i].set_xticks(tick_list)
+                axs[i].set_xticks(tick_list)
             else:
-                axs1[i].set_xticks(tick_list[:-1])
+                axs[i].set_xticks(tick_list[:-1])
 
-        fig1.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+        fig.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
     
     if save:
-        save_plots(fig1, xy, logged)
+        save_plots(fig, xy, logged)
 
     if show:
         plt.show()
@@ -198,19 +228,18 @@ def plot_regression():
 
     
 def save_plots(figure, xy, logged):
-    """
-    Saves simple/log plots to respective folders.
+    """Saves simple/log plots to respective folders.
 
-    Save figures to respective 'Saved Simple Plots' or 'Saved Log Plots' folders in the current directory, 
-    depending on if 'logged' is True. Each figure's save file is named as such:
+    Each figure's save file is named as such:
     '(number of plots in figure) Simple Plot(s) - #(number denoting order in which file was saved).
+
     SIMPLE_PLOT_DETAILS.txt or LOG_PLOT_DETAILS.txt files also created containing number of plots, number
-    denoting order save file, independent and dependent variables used for plotting, and figure creation time. 
-    
-    Parameters:
-    figure (Matplotlib figure): the current figure object defined within plot_variables().
-                                xy (tuple): tuple of tuples, each containing independent/dependent variable pairs.
-    logged (bool): determines creation of simple plot (False), or log plot save folders (True).
+    denoting order save file, value of `xy`, and figure creation time. 
+
+    Args:
+        figure (Matplotlib figure): the current figure object defined within plot_variables().
+        xy (tuple): tuple of tuples, each containing independent/dependent variable pairs.
+        logged (bool): determines creation of 'Saved Simple Plots' (False), or 'Saved Log Plots' folders (True).
     """
     while True:
         if not logged:
@@ -231,11 +260,11 @@ def save_plots(figure, xy, logged):
                     png_id += 1
                 figure.savefig(f'Saved Simple Plots/{len(xy)} Simple Plot(s) - #{png_id:d}.png')
 
-            var_list = [x for x in xy]
+            var_list = "\n".join(str(x) for x in xy)
             with open(f'Saved Simple Plots/SIMPLE_PLOT_DETAILS.txt', 'a') as save_details:
                 save_details.write(
                     f'{len(xy)} Simple Plot(s) - #{png_id:d}'
-                    f' - {*var_list,}\n'
+                    f' - {var_list}\n'
                     f' - Figure Created on {datetime.now().strftime("%d-%m-%Y at %H:%M:%S")}\n\n'
                     )
                 
@@ -260,12 +289,12 @@ def save_plots(figure, xy, logged):
                     png_id += 1
                 figure.savefig(f'Saved Log Plots/{len(xy)} Log Plot(s) - #{png_id:d}.png')
 
-            var_list = [x for x in xy]
+            var_list = "\n".join(str(x) for x in xy)
             with open(f'Saved Log Plots/LOG_PLOT_DETAILS.txt', 'a') as save_details:
                 save_details.write(
                     f'{len(xy)} Log Plot(s) - #{png_id:d}'
-                    f' - {*var_list,}\n'
-                    f' - Figure Created on {datetime.now().strftime("%d-%m-%Y at %H:%M:%S")}\n\n'
+                    f'\n{var_list}\n'
+                    f'- Figure Created on {datetime.now().strftime("%d-%m-%Y at %H:%M:%S")}\n\n'
                     )
                 
                 print(f'Log Plots saved to {os.path.join(os.getcwd(), r"Saved Log Plots")}')
@@ -273,11 +302,10 @@ def save_plots(figure, xy, logged):
 
 
 def delete_folder(logged=False):
-    """
-    Deletes simple or log save folder depending on if logged=True is passed as an argument.
-    
-    Parameters:
-    logged (bool): determines deletion of simple plot (False), or log plot save folders (True).
+    """Deletes simple or log save folder depending on if logged=True is passed as an argument.
+
+    Args:
+        logged (bool): determines deletion of simple plot (False), or log plot save folders (True).
     """
     if not logged:
         folder = os.path.join(os.getcwd(), r'Saved Simple Plots')
@@ -292,11 +320,13 @@ def delete_folder(logged=False):
 
 
 if __name__ == '__main__':
-    # plot_variables((('Cerebellum Surface Area', 'Cerebellum Volume'),), logged=True, show=True)
     # plot_variables((('Cerebellum Surface Area', 'Cerebellum Volume'),),)
 
-    # plot_variables(logged=True)
-    plot_variables(logged=False, show=True)
-    
+    plot_variables(show=True)
+    plot_variables(colors={'Hominidae':'blue'}, show=True)
+
+    set_colors({'Hominidae':'red'})
+    plot_variables(show=True)
+
+    # delete_folder(logged=True)
     # plot_regression()
-    # show_plots()
